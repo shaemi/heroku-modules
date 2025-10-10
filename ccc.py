6a49a7de1dd7b30069fae17df5f24d1d740f9b88
Alireza Shaemi <61411316+shaemi@users.noreply.github.com>
2025-10-10 10:40:20 +0330

    Create ccc.py

---
+++ ccc.py
@@ -0,0 +1,51 @@
+version = (1, 0, 0)
+
+# by: aliultraa.t.me
+
+import random
+from datetime import timedelta
+from telethon import events
+from telethon import functions
+from telethon.tl.types import Message
+from .. import loader, utils
+
+bot = "@CopilotOfficialBot"
+bot_id = 7147619821
+
+@loader.tds
+class CopilotAIMod(loader.Module):
+    """AI Assist for @CopilotOfficialBot"""
+
+    strings = {
+        "name": "CopilotAI",
+    }
+            	
+    @loader.command()
+    async def copilotdelcmd(self, message):
+        """- Clear chat history."""
+        chat = bot_id
+        text = "/newchat"
+        async with message.client.conversation(bot) as conv:
+            response = await conv.send_message(text)
+            response1 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+            await utils.answer(message, "✅ <b>History cleared!</b>")
+            await response.delete()
+            await response1.delete()
+           
+           
+@loader.command()
+    async def copilotcmd(self, message):
+        """<текст> - Ask @CopilotOfficialBot"""
+        chat = bot_id
+        reply = await message.get_reply_message()
+        text = reply.raw_text if reply else utils.get_args_raw(message)
+        if len(text) < 3:
+        	await utils.answer(message, "🚫<b>Error!\nyour message is to short.</b>")
+        	return
+        await utils.answer(message, "🤖<b>AI is answring...</b>")
+        async with message.client.conversation(bot) as conv:
+            response = await conv.send_message(text)
+            response1 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+            await utils.answer(message, f"❓<b>Question:</b> \n{text}\n\n🤖 <b>Copilot answer:</b>\n{response1.text}")
+            await response.delete()
+            await response1.delete()
